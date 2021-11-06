fdfdffc3a4ec77d8e504042712c7ba90a6e344c1
Natalie Lane <nlane@udel.edu>
2021-11-06 19:43:02 -0400

    adds alt tags

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="content">
-      <img className="gif" src="https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif" />
+      <img className="gif" src="https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif" alt="bear-waving-hello"/>
     </div>
     );
 }

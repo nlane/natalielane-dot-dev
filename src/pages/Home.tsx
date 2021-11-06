5fb3c27eea60e72158b9600f8f78c2c469755ffc
Natalie Lane <nlane@udel.edu>
2021-11-06 19:50:27 -0400

    adjust media sizing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="content">
-      <img className="gif" src="https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif" alt="bear-waving-hello"/>
+      <img className="media" src="https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif" alt="bear-waving-hello"/>
     </div>
     );
 }
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="content">
       <h2>This is where a post would go...</h2>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F60%2F53%2F95%2F6053954a3dd4ff104743099a1d7ccec3--cartoons-parents.jpg&f=1&nofb=1" alt="if-i-had-any-meme" />
+      <img className="media" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F60%2F53%2F95%2F6053954a3dd4ff104743099a1d7ccec3--cartoons-parents.jpg&f=1&nofb=1" alt="if-i-had-any-meme" />
     </div>
   );
 }

5877baee3e48ec6768c77535253f5691440f9e7d
Natalie Lane <nlane@udel.edu>
2022-02-20 16:56:52 -0500

    new design

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
-import { Home, About, Posts } from './pages';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { Home, About, Posts } from "./pages";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <nav>
-        <h1>Natalie Lane</h1>
-        <div className="link-group">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/posts">Posts</Link>
-        </div>
-      </nav>
-      <main>
+        <nav>
+          <span id="logo">Natalie Lane</span>
+          <div className="link-group">
+            <Link to="/" className="link-border">
+              Home
+            </Link>
+            <Link to="/about">About</Link>
+            <Link to="/posts" className="link-border">
+              Posts
+            </Link>
+          </div>
+        </nav>
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/posts" element={<Posts />} />
           </Routes>
-          </main>
+        </main>
       </div>
     </Router>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
-    <div className="content">
-      <img className="media" src="https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif" alt="bear-waving-hello"/>
+    <div>
+      <div className="greetings">
+        <p className="name">Hi, I'm Natalie.</p>
+        <p className="name">I come in peace ✌️</p>
+      </div>
+      <img className="alien" src="alien.PNG" alt="alien in flying saucer" />
     </div>
-    );
-}
+  );
+};
 
 export default Home;
